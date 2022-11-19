b8a14b5c5b4f6cb5ac43f0e2809e2246fda6fa22
Eloi Le Renard <77073619+Eloi-Le-Renard@users.noreply.github.com>
2022-11-19 22:39:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,5 @@ app = Flask(__name__)
 def home():
     return 'Hello World'
 
-@app.route('/predict_api', methods=["GET","POST"])
-def list_post():
-    json_body = request.get_json()
-    predictions = 2 * json_body[0]   
-    predictions = list(predictions)
-    return jsonify(results = predictions)
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
